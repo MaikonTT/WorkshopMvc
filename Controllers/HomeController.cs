ea99a1823f9aefc4332336173a9654c5d4e4a1b0
Maikon Takeshi Tsujiguchi <maikon_numb@hotmail.com>
2021-06-13 12:43:52 -0300

    Adicionado rota inicial e rota contato

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace WorkshopMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "Hello World";
+        }
+
+        public string Contato()
+        {
+            return "Entre em contato!";
+        }
+    }
+}
